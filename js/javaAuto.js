aa67b9f3c94532b64ac42a59eed812ba9e87a686
xghg <dhyr@qq.com>
2016-09-21 19:58:01 +0800

    third

---
+++ js/javaAuto.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Administrator on 16-5-8.
+ */
+function fontAuto(width){//width就是效果图的宽度
+    var winW=document.documentElement.clientWidth;//屏幕宽度
+    if(winW>=width)
+    {
+        document.documentElement.style.fontSize="625%";
+    }
+    else{
+        document.documentElement.style.fontSize=(winW/width*625)+"%";
+    }
+}
+fontAuto(640);
+
+window.onresize=function(){//窗口发生变化的时候触发函数
+    fontAuto(640);
+}
